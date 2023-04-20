bbde6ac40826af85ce7939dabdbafdb3aa376f5f
michellesylee <michellebellelee@yahoo.com>
2023-04-20 13:43:22 -0700

    stashing changes to pull from demo branch

--- src/cRender.js
+++ src/cRender.js
@@ -1,9 +1,9 @@
-import ReactReconciler from 'react-reconciler';
+import ReactReconciler from "react-reconciler";
 import {
   DiscreteEventPriority,
   ContinuousEventPriority,
   DefaultEventPriority,
-} from 'react-reconciler/constants';
+} from "react-reconciler/constants";
 
 const isSuspense = (fiber) => {
   let count = 0;
@@ -16,76 +16,71 @@ const isSuspense = (fiber) => {
 };
 
 const getLaneNum = (fiber) => {
-    let count = 0; 
+  let count = 0;
   //let lanes = []
   while (fiber && count < 5) {
-    if (fiber.lanes !== null ) {
-      const laneNum = Math.log2(fiber.lanes); 
+    if (fiber.lanes !== null) {
+      const laneNum = Math.log2(fiber.lanes);
       //lanes.push(fiber.lanes)
-      //TODO: clean this logic; 
-      //make the lanes more fine grained; a color for a type of lane; a shade for a specific lane of that type 
+      //TODO: clean this logic;
+      //make the lanes more fine grained; a color for a type of lane; a shade for a specific lane of that type
       //probably will need bitwise operatitions
-      if(laneNum > -Infinity) {
-        console.log(`We found a special lane: ${laneNum} in ancestor ${count}`); 
-        return laneNum; 
+      if (laneNum > -Infinity) {
+        console.log(`We found a special lane: ${laneNum} in ancestor ${count}`);
+        return laneNum;
       }
     }
     fiber = fiber.return;
     count++;
   }
-  return 0; 
-
-}
+  return 0;
+};
 
 const isTransition = (laneNum) => {
-    return laneNum >= 6 && laneNum <= 22
-}
-
+  return laneNum >= 6 && laneNum <= 22;
+};
 
 const setTransitionColor = (laneNum) => {
-    switch(laneNum) {
-        case 6: 
-            return "#99e2b4"; 
-        case 7: 
-            return "#88d4dB"; 
-        case 8: 
-            return "#9ff7cb"; 
-        case 9: 
-            return "#67b99a"; 
-        case 10: 
-            return "#56ab91"; 
-        case 11: 
-            return "#469d89"; 
-        case 12: 
-            return "#358f80";
-        case 13: 
-            return "#248277"; 
-        case 14: 
-            return "#14746f";
-        case 15: 
-            return "#036666";
-        case 16: 
-            return "#40916c"; 
-        case 17: 
-            return "#25a244";
-        case 18: 
-            return "#208b3a";
-        case 19: 
-            return "#1a7431";
-        case 20: 
-            return "#155d27";
-        case 21: 
-            return "#10451d"; 
-        case 22: 
-            return "#2d6a4f"; 
-        
-        default: 
-            return "#2d6a4f"; 
-        
-    }
+  switch (laneNum) {
+    case 6:
+      return "#99e2b4";
+    case 7:
+      return "#88d4dB";
+    case 8:
+      return "#9ff7cb";
+    case 9:
+      return "#67b99a";
+    case 10:
+      return "#56ab91";
+    case 11:
+      return "#469d89";
+    case 12:
+      return "#358f80";
+    case 13:
+      return "#248277";
+    case 14:
+      return "#14746f";
+    case 15:
+      return "#036666";
+    case 16:
+      return "#40916c";
+    case 17:
+      return "#25a244";
+    case 18:
+      return "#208b3a";
+    case 19:
+      return "#1a7431";
+    case 20:
+      return "#155d27";
+    case 21:
+      return "#10451d";
+    case 22:
+      return "#2d6a4f";
 
-}
-
+    default:
+      return "#2d6a4f";
+  }
+};
 
 const reconciler = ReactReconciler({
   createInstance: (
@@ -96,30 +91,27 @@ const reconciler = ReactReconciler({
     internalInstanceHandle
   ) => {
     //console.log(type, props);
-    console.log('Fiber: ', internalInstanceHandle);
-    
-   
+    // console.log('Fiber: ', internalInstanceHandle);
+
     const element = document.createElement(type);
 
     //for fun now; maybe useful later (e.g., display some useful info)
 
-    if (props.id === 'app') {
-      const header = document.createElement('h1'); 
-      const container = document.createElement('div'); 
-      header.innerText = 'Rendered by C-React';
-      header.style.color = "#0077B6"; 
-      header.style.font = 'bold 25px impact,serif'
-      container.style.backgroundColor = '#E9C46A'
-      container.style.border = 'solid black';
-      container.style.marginBottom = '20px'; 
+    if (props.id === "app") {
+      const header = document.createElement("h1");
+      const container = document.createElement("div");
+      header.innerText = "Rendered by C-React";
+      header.style.color = "#0077B6";
+      header.style.font = "bold 25px impact,serif";
+      container.style.backgroundColor = "#E9C46A";
+      container.style.border = "solid black";
+      container.style.marginBottom = "20px";
       container.appendChild(header);
-      element.prepend(container)
-      
+      element.prepend(container);
     }
 
-
     //TODO: simplify this, using a loop over props (Object.keys(props)) or something
-    element.className = props.className || '';
+    element.className = props.className || "";
 
     // check if the prop has incline styles; if yes, add the style to element
     // element.style = props.style  //==> this does not work; the following does
@@ -131,26 +123,30 @@ const reconciler = ReactReconciler({
     }
 
     if (props.onClick) {
-      element.addEventListener('click', props.onClick);
+      element.addEventListener("click", props.onClick);
     }
     if (props.onChange) {
-      element.addEventListener('input', props.onChange)
+      element.addEventListener("input", props.onChange);
     }
 
-    if (props.id) element.id = props.id; 
+    if (props.id) element.id = props.id;
 
     if (props.src) element.src = props.src;
-    if (type === 'button') element.style.borderColor = 'red';
+    if (type === "button") element.style.borderColor = "red";
 
     // check if has suspense ancestor
-    if (isSuspense(internalInstanceHandle))
-      element.style.backgroundColor = '#E0FFFF';
+    if (isSuspense(internalInstanceHandle)) {
+      element.style.backgroundColor = "#E0FFFF";
+      console.log("this is suspense");
+    }
 
     //check if has useTransition ancestor
-    const laneNum = getLaneNum(internalInstanceHandle); 
+    const laneNum = getLaneNum(internalInstanceHandle);
     if (isTransition(laneNum)) {
       element.style.backgroundColor = setTransitionColor(laneNum);
-      element.classList.add('TransitionLane'+ (laneNum - 6));  
+      element.classList.add("TransitionLane" + laneNum);
+
+      // element.classList.add("TransitionLane" + (laneNum - 6));
     }
 
     return element;
@@ -202,11 +198,11 @@ const reconciler = ReactReconciler({
     hostContext
   ) => {
     // console.log("Prepare Update");
-    // console.log("Old Props", oldProps); 
+    // console.log("Old Props", oldProps);
     // console.log("New Props", newProps)
     // console.log("rootContainer", rootContainer);
-    // console.log("hostContext", hostContext); 
-    return; 
+    // console.log("hostContext", hostContext);
+    return;
   },
   commitUpdate: (
     instance,
@@ -216,11 +212,11 @@ const reconciler = ReactReconciler({
     nextProps,
     internalHandle
   ) => {
-    console.log("Commit Update"); 
-    console.log("instanceHandle", internalHandle); 
+    console.log("Commit Update");
+    console.log("instanceHandle", internalHandle);
     if (isTransition(internalHandle)) {
       instance.style.backgroundColor = "#66ff99";
-      instance.classList.add('c-Transition');  
+      instance.classList.add("c-Transition");
     }
   },
   commitTextUpdate: (textInstance, oldText, newText) => {
@@ -231,23 +227,27 @@ const reconciler = ReactReconciler({
   shouldDeprioritizeSubtree: (type, nextProps) => {
     return !nextProps.hidden;
   },
-  detachDeletedInstance: () => {}, 
-  getCurrentEventPriority: ()=> {
+  detachDeletedInstance: () => {},
+  getCurrentEventPriority: () => {
     //TODO:understand what these mean and if they are useful
-    //current obsveration: the 3 values are different but remain constant through updates [1, 4, 16] 
-    console.log([DiscreteEventPriority,
+    //current obsveration: the 3 values are different but remain constant through updates [1, 4, 16]
+    console.log([
+      DiscreteEventPriority,
       ContinuousEventPriority,
-      DefaultEventPriority]); 
-    return DefaultEventPriority 
+      DefaultEventPriority,
+    ]);
+    return DefaultEventPriority;
   },
-  commitMount: (instace, type, props, internalInstanceHandle)=> {console.log("Commit Mount ", internalInstanceHandle)},
+  commitMount: (instace, type, props, internalInstanceHandle) => {
+    console.log("Commit Mount ", internalInstanceHandle);
+  },
   supportsMutation: true,
 });
 
 const cRender = (element, container) => {
   // console.log("Container: ", container);
   const fiberRoot = reconciler.createContainer(container, 1, true, true);
-  console.log('Root: ', fiberRoot);
+  console.log("Root: ", fiberRoot);
   reconciler.updateContainer(element, fiberRoot, null, null);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import cRender from './cRender.js'
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import cRender from "./cRender.js";
+import "./index.css";
+import App from "./App";
+import cRender from "creact-visualizer";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
 // );
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
-cRender(<React.StrictMode>
+cRender(
+  <React.StrictMode>
     <App />
-    </React.StrictMode>, root)
-
+  </React.StrictMode>,
+  root
+);
